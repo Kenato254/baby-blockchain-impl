1a6818555e8b2cde919bdd7508767a2f0bb4cd7f
Kenato254 <kenedygitonga@gmail.com>
2022-07-13 02:15:02 +0300

    Add Asset method

--- Account/Account.py
+++ Account/Account.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from base64 import b64encode, b16decode
 from hashlib import sha256, sha512
 from pprint import pprint
 from dataclasses import dataclass, field, asdict
@@ -36,9 +37,11 @@ class Account:
         Structure["PrivateKey: Object, PublicKey: Object, n_value: Object"],
     ] | None = None
     __balance: int = 0
-    assets: NDArray[
+    __assets: NDArray[
         Shape["1,0"],
-        Structure["DigitalDeed: Str, location: Str, worth: UInt64, description: Str"],
+        Structure[
+            "digital_deed: Object, appro_area: Object, worth: Object, owner: Object"
+        ],
     ] | None = None
 
     @classmethod
@@ -85,7 +88,7 @@ class Account:
         self, recipient: "Account", amount: int, index: int
     ) -> Operation:
         """
-        a function that allows to create a payment operation on behalf of this account to the recipient. 
+        a function that allows to create a payment operation on behalf of this account to the recipient.
 
         :recipient: Account object as input to which the payment will be made.
         :amount: the transfer amount.
@@ -99,15 +102,15 @@ class Account:
         """
         a function that allows to get the state of the user's balance.
 
-        :return: None
+        :return: integer
         """
         return self.__balance
 
     @get_balance.setter
     def update_balance(self, value: int) -> None:
         """
-        a function that allows to update the state of the user's balance. 
-        
+        a function that allows to update the state of the user's balance.
+
         :value: an integer input.
         :return: None
         """
@@ -117,20 +120,53 @@ class Account:
     @get_balance.getter
     def print_balance(self) -> None:
         """
-        a function that allows to display the state of the user's balance. 
+        a function that allows to display the state of the user's balance.
 
         :return: None
         """
         print(f"Account balance: {self.__balance}")
 
+    def create_assets(self, deed_no: bytes, appro_area: bytes, worth: int) -> None:
+        """
+        a function creates a new assets.
+
+        :deed_no: a deed number that will be turned into a digital deed. It designates ownership of a property
+        :appro_area: approximate area of the property/land
+        :worth: cost of the property
+        :owner: account_id of the current owner
+        """
+        data_struct = np.dtype(
+            [
+                ("digital_deed", "O"),
+                ("appro_area", "O"),
+                ("worth", "O"),
+                ("owner", "O"),
+            ]
+        )
+        temp = np.array(
+            [
+                (
+                    b64encode(deed_no).hex(),
+                    b64encode(appro_area).hex(),
+                    b64encode(worth).hex(),
+                    self.account_id,
+                )
+            ],
+            dtype=data_struct,
+        )
+        
+        if self.__assets is not None:
+            self.update_assets = temp
+        self.__assets = temp
+
     @property
     def get_assets(self):
         """
-        a function that allows to get the state of the user's balance. 
+        a function that allows to get the state of the user's balance.
 
         :return: None
         """
-        return self.assets
+        return self.__assets
 
     @get_assets.setter
     def update_assets(
@@ -138,18 +174,21 @@ class Account:
         asset: NDArray[
             Shape["1,0"],
             Structure[
-                "DigitalDeed: Str, location: Str, worth: UInt64, description: Str"
+                "digital_deed: Object, appro_area: Object, worth: Object, owner: Object"
             ],
         ],
     ) -> None:
         """
         a function that allows to update the state of the user's assets.
-        
+
         :asset: An Array consisting of information of a property.
         :return: None
         """
-        # TODO: Needs Transaction Class.
-        ...
+        if self.account_id == asset["owner"][0]:
+            if self.__assets is None:
+                self.__assets = asset
+            else:
+                self.__assets = np.append(self.__assets, asset)
 
     @get_assets.getter
     def print_assets(self) -> None:
@@ -158,18 +197,22 @@ class Account:
 
         :return: None
         """
-        pprint(f"""
-        Deed: {self.assets['digitalDeed']}
-        Location: {self.assets['location']}
-        Worth: {self.assets['worth']}
-        Description: {self.assets['description']}
-        """
-        )
+        if self.get_assets is not None:
+            print(
+                f"""
+            Deed: {self.get_assets['digital_deed']}
+            Approximate Area: {self.get_assets['appro_area']}
+            Worth: {self.get_assets['worth']}
+            owner: {self.get_assets['owner'][0]}
+            """
+            )
+        else:
+            raise ValueError("You have no assets available.")
 
     def sign_data(self, msg: bytes, idx: int) -> bytes:
         """
         a function that allows the user to sign random data. It accepts a message and an index of the key pair in the
-        wallet as input. 
+        wallet as input.
 
         :return: bytes -> The value of the signature
         """
@@ -179,7 +222,7 @@ class Account:
 
     def to_string(self) -> str:
         """
-        a function that allows to form a string with an account object. 
+        a function that allows to form a string with an account object.
 
         :return: an object of the String class.
         """
@@ -192,7 +235,7 @@ class Account:
 
     def print(self) -> None:
         """
-        a function for output key pair objects. 
+        a function for output key pair objects.
 
         :return: None
         """
@@ -204,8 +247,12 @@ if __name__ == "__main__":
     acc = account.gen_account()
     acc.add_key_pair_to_wallet(KeyPair())
     randata: bytes = b"some random data to be signed."
+
+    #! https://upload.wikimedia.org/wikipedia/commons/8/80/Example_of_a_blank_Kenyan_Deed_Title.png
+    acc.create_assets(b"KAJIADO/LOODARIAK/579", b"104.0 hectares", b"1000000")
+    acc.create_assets(b"KAJIADO/LOODARIAK/579", b"80.0 hectares", b"1000000")
     # acc.sign_data(randata, 1)
     # print(acc.to_string())
     # acc.to_string()
     # acc.print()
-    acc.print_assets()
+    acc.print_assets
